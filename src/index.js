73eff93a0e3adf8752ef08da55dc2215f58f261c
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2024-08-03 13:28:50 -0400

    Implement html fetching service w/ unit tests

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("hello, node!");
+import HTMLFetchingService from "./services/htmlFetchingService.js";
+
+async function run() {
+  const url = "https://www.lockheedmartinjobs.com/search-jobs";
+  const fetcher = new HTMLFetchingService();
+
+  try {
+    // Fetch HTML content
+    const html = await fetcher.fetchHTML(url);
+    console.log("HTML content fetched successfully. Printing:");
+    console.log(html);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+run();
